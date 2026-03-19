9dc82c5081ee2386cc1da8a5c674d8469d58f944
Inan Tubek <inantubek@icloud.com>
2026-03-19 07:36:31 +0300

    Initial Commit

---
+++ SMSpam/SMSpamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMSpamApp.swift
+//  SMSpam
+//
+//  Created by Inan Tubek on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SMSpamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
